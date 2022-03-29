094c074221204a04e0ff3e20df6293d248c86651
yonatan.altman <yonatan@yaltman.com>
2022-03-29 22:41:28 +0300

    initial commit

---
+++ apps/cisco-meetup/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@faster-with-plugins/api-interfaces';
+
+@Component({
+  selector: 'faster-with-plugins-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
